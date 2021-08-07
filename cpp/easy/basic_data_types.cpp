de4f50c28ec601e871384a067e07cf901075fb82
bheki-maenetja <bhekimaenetja@gmail.com>
2021-08-07 12:37:40 +0200

    completed first few problems for C++

---
+++ cpp/easy/basic_data_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char ch;
+    float fl;
+    double db;
+    scanf("%i %ld %c %f %lf", &i, &l, &ch, &fl, &db);
+    printf("%i\n%ld\n%c\n%f\n%lf", i , l , ch, fl, db);
+    return 0;
+}
---
+++ cpp/easy/input_and_output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int nums[3];
+    cin >> nums[0] >> nums[1] >> nums[2];
+    cout << nums[0] + nums[1] + nums[2] << endl; 
+    return 0;
+}
